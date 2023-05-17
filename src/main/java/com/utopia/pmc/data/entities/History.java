5f4ba9d532c1fedfaadd35e350a9134c2fd87872
DangHuuDat <danghuudat163@gmail.com>
2023-05-17 21:28:45 +0700

    Create History Entity

---
+++ src/main/java/com/utopia/pmc/data/entities/History.java
@@ -0,0 +1,49 @@
+package com.utopia.pmc.data.entities;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.utopia.pmc.data.constants.statuses.TakenStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "History")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class History {
+    @Id
+    @SequenceGenerator(name = "history_sequence", sequenceName = "history_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "history_sequence")
+    private long id;
+    @Column
+    private LocalDate dateTaken;
+    @Column
+    private LocalTime timeTaken;
+    @Column
+    private TakenStatus takenStatus;
+    @ManyToOne
+    @JoinColumn(name = "regiment_id")
+    private Regiment regiment;
+    @ManyToOne
+    @JoinColumn(name = "medicine_id")
+    private Medicine medicine;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; 
+}
