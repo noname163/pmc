d4f760b325ec8b926f8a0179dbafce30ab42aeb3
DangHuuDat <danghuudat163@gmail.com>
2023-05-17 21:31:22 +0700

    Create CustomUserDetails

---
+++ src/main/java/com/utopia/pmc/config/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package com.utopia.pmc.config;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.utopia.pmc.data.constants.others.Role;
+import com.utopia.pmc.data.entities.User;
+
+public class CustomUserDetails extends User implements UserDetails {
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+    private Role role;
+    private final transient User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    public User getUser() {
+        return this.user;
+    }
+}
