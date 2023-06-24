1b56913d8aaedcfc182882a1cc8977766377a99c
DangHuuDat <danghuudat163@gmail.com>
2023-06-24 11:28:01 +0700

    hotfix: remove baseUrl in PaymentServiceImpl into EnvironmentVariable

---
+++ src/main/java/com/utopia/pmc/utils/EnvironmentVariable.java
@@ -0,0 +1,17 @@
+package com.utopia.pmc.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Component
+@Getter
+public class EnvironmentVariable {
+    @Value("${jwt.secret-key}")
+    private String jwtSecret;
+    @Value("${jwt.expires-time}")
+    private long expiresTime;
+    @Value("${base.url}")
+    private String baseurl;
+}
