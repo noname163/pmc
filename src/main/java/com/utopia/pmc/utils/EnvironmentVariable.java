af2a7e5150c6b4ef3189b9b3e038ddc27c914020
DangHuuDat <danghuudat163@gmail.com>
2023-06-26 10:24:09 +0700

    remove unuse code

--- src/main/java/com/utopia/pmc/utils/EnvironmentVariable.java
+++ src/main/java/com/utopia/pmc/utils/EnvironmentVariable.java
@@ -12,6 +12,5 @@ public class EnvironmentVariable {
     private String jwtSecret;
     @Value("${jwt.expires-time}")
     private long expiresTime;
-    // @Value("${app.base-url}")
-    // private String baseurl;
+   
 }
