f1c71e935b64605fdecc7e450e79a462c7f71589
DangHuuDat <danghuudat163@gmail.com>
2023-07-23 11:04:24 +0700

    Hotfix(PMC-33): change import od RequestBody

--- src/main/java/com/utopia/pmc/controller/admin/PaymentController.java
+++ src/main/java/com/utopia/pmc/controller/admin/PaymentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,6 @@ import com.utopia.pmc.services.payment.PaymentPlansService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
