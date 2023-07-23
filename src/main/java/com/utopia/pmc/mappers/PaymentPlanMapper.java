712b590133cbe64d259f1f817d7b550888c67b94
DangHuuDat <danghuudat163@gmail.com>
2023-07-23 10:15:26 +0700

    Hotfix(PMC-33): Add create payment plan api

--- src/main/java/com/utopia/pmc/mappers/PaymentPlanMapper.java
+++ src/main/java/com/utopia/pmc/mappers/PaymentPlanMapper.java
@@ -5,26 +5,39 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
+import com.utopia.pmc.data.dto.request.payment.NewPaymentRequest;
 import com.utopia.pmc.data.dto.response.payment.PaymentPlanResponse;
 import com.utopia.pmc.data.dto.response.payment.PaymentResponse;
 import com.utopia.pmc.data.entities.PaymentPlan;
 
 @Component
 public class PaymentPlanMapper {
-    public PaymentPlanResponse mapEntityToDto(PaymentPlan paymentPlan){
+    public PaymentPlanResponse mapEntityToDto(PaymentPlan paymentPlan) {
         return PaymentPlanResponse
-        .builder()
-        .id(paymentPlan.getId())
-        .name(paymentPlan.getName())
-        .money(paymentPlan.getMoney())
-        .numberOfMedicine(paymentPlan.getNumberOfMedicine())
-        .numberOfRegiment(paymentPlan.getNumberOfRegiment())
-        .expriredTime(paymentPlan.getExpriredTime())
-        .period(paymentPlan.getPeriod())
-        .build();
+                .builder()
+                .id(paymentPlan.getId())
+                .name(paymentPlan.getName())
+                .money(paymentPlan.getMoney())
+                .numberOfMedicine(paymentPlan.getNumberOfMedicine())
+                .numberOfRegiment(paymentPlan.getNumberOfRegiment())
+                .expriredTime(paymentPlan.getExpriredTime())
+                .period(paymentPlan.getPeriod())
+                .build();
     }
 
-    public List<PaymentPlanResponse> mapEntitiesToDtos(List<PaymentPlan> paymentPlans){
+    public PaymentPlan mapDtoToEntity(NewPaymentRequest newPaymentRequest) {
+        return PaymentPlan
+                .builder()
+                .name(newPaymentRequest.getName())
+                .money(newPaymentRequest.getMoney())
+                .numberOfMedicine(newPaymentRequest.getNumberOfMedicine())
+                .numberOfRegiment(newPaymentRequest.getNumberOfRegiment())
+                .expriredTime(newPaymentRequest.getExpriredTime())
+                .period(newPaymentRequest.getPeriod())
+                .build();
+    }
+
+    public List<PaymentPlanResponse> mapEntitiesToDtos(List<PaymentPlan> paymentPlans) {
         return paymentPlans.stream().map(this::mapEntityToDto).collect(Collectors.toList());
     }
 }
